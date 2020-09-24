a997b4e00922ceef40f94162bc8a8d943fe8ec47
Fernando Blat <fernando@blat.es>
2020-09-24 18:17:08 +0200

    Typo

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -5,7 +5,7 @@ Decidim.configure do |config|
   config.application_name = "Participa"
 
   # The email that will be used as sender in all emails from Decidim
-  config.mailer_sender = "participa@mairenadelalcor.es "
+  config.mailer_sender = "participa@mairenadelalcor.es"
 
   # Sets the list of available locales for the whole application.
   #
